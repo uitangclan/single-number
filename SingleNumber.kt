2b4f72b53e347a68bd65fd52a8763c1d87c88197
uitangclan <uitangclan@gmail.com>
2024-09-09 20:42:40 +0900

    happy codeing

--- SingleNumber.kt
+++ SingleNumber.kt
@@ -1,20 +1,18 @@
 fun main (){
-    // val nums = intArrayOf(4,1,2,1,2)
+    val nums = intArrayOf(4,1,2,1,2)
     // val nums = intArrayOf(1)
-    val nums = intArrayOf(2, 2, 1)
+    // val nums = intArrayOf(2, 2, 1)
     println(singleNumber(nums))
 }
 
 fun singleNumber(nums: IntArray):Int? {
-    val map = HashMap<Int, Int> ()
-    for (i in nums.indices) {
-        map.put(nums[i], i)
-        println(!map.containsKey(1))
-    }
-    for (i in nums.indices) {
-        if(!map.containsKey(1)) {
-            return 500
+    var list = mutableListOf<Int> ()
+    for (i in nums) {
+        if (!list.contains(i)) {
+            list.add(i)
+        } else {
+            list.remove(i)
         }
     }
-    return null
+    return list[0]
 }
