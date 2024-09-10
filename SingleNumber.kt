a285a16bc2e161b2f0f93a8e362a163ca8dedd55
uitangclan <uitangclan@gmail.com>
2024-09-10 19:54:56 +0900

    happy coding

--- SingleNumber.kt
+++ SingleNumber.kt
@@ -1,5 +1,6 @@
 fun main (){
-    val nums = intArrayOf(4,1,2,1,2)
+    // val nums = intArrayOf(4,1,2,1,2)
+    val nums = intArrayOf(1,2,3,1,2)
     // val nums = intArrayOf(1)
     // val nums = intArrayOf(2, 2, 1)
     println(singleNumber(nums))
@@ -7,16 +8,10 @@ fun main (){
 
 fun singleNumber(nums: IntArray):Int {
 
-    var set = HashSet<Int>()
-    var j = 0
-    var k = 0
-
+    var a = 0
     for (i in nums) {
-        if (!set.contains(i)) {
-            set.add(i)
-            j += i
-        }
-        k += i
+        a = a xor i
+        println(a)
     } 
-    return 2 * j -k
+    return a
 }
